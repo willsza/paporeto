3b51f4f35ae23083a3bb6540b49dd28d4d286003
wilfrank <wilfrank84@gmail.com>
2015-02-12 20:37:56 -0200

    Forçando variaveis uploadbox.rb como string

--- config/initializers/uploadbox.rb
+++ config/initializers/uploadbox.rb
@@ -19,8 +19,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      Rails.application.secrets.s3_key,
-    aws_secret_access_key:  Rails.application.secrets.s3_secret
+    aws_access_key_id:      "#{Rails.application.secrets.s3_key}",
+    aws_secret_access_key:  "#{Rails.application.secrets.s3_secret}"
   }
 
   config.fog_directory  = Rails.application.secrets.s3_bucket
